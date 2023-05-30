be69f9075d3c8c88114f72e7b24613257bc0ee4e
Reshma-Ghosh <reshmaghosh114@gmail.com>
2023-05-30 08:40:52 +0200

    producl-List

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import ProductList from "./components/products/ProductList";
 
 function App() {
   return (
     <div className="App">
       <h1> React assignment2</h1>
+      <ProductList />
     </div>
   );
 }
--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -1,5 +1,36 @@
-import React from "react";
+import { error } from "console";
+import React, { useEffect, useState } from "react";
 
-export default function ProductList() {
-  return <div>ProductList</div>;
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+};
+
+export default function ProductList(): JSX.Element {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => response.json())
+      .then((data: Product[]) => {
+        setProducts(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching", error);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>Product List</h1>
+      <div>
+        {products.map((product) => (
+          <div key={product.id}>
+            <p>{product.title}</p>
+            <p>{product.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
